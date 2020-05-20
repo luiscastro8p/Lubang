cc7107a0da07d433ac65d8c869faddf3ff7d2b5b
luiscastro8p <luisarmandocastro12@gmail.com>
2020-05-20 14:44:15 -0600

    [UPDATE]Se agrego pantalla de reporte con imagen

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -20,13 +20,13 @@ export class ListPage implements OnInit {
     "build",
   ];
   temp = [];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public items: Array<{ title: string; note: string; icon: any }> = [];
   constructor(public DataService: AuthService) {
     for (let i = 1; i < 55; i++) {
       this.items.push({
         title: "10/20/1 " + i,
         note: "This is item #" + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)],
+        icon: Math.floor(Math.random() * 3),
       });
     }
   }
--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.page.html',
-  styleUrls: ['./report.page.scss'],
+  selector: "app-report",
+  templateUrl: "./report.page.html",
+  styleUrls: ["./report.page.scss"],
 })
 export class ReportPage implements OnInit {
+  imageURL;
+  constructor(public camera: Camera) {}
 
-  constructor() { }
+  ngOnInit() {}
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
 
-  ngOnInit() {
+    this.camera.getPicture(options).then(
+      (imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        let base64Image = "data:image/jpeg;base64," + imageData;
+
+        this.imageURL = base64Image;
+      },
+      (err) => {
+        // Handle error
+      }
+    );
   }
-
 }
