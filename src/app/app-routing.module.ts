376a6ad81470c63f5f78e68d43e4595215c8f745
luiscastro8p <luisarmandocastro12@gmail.com>
2020-05-19 21:34:55 -0600

    [UPDATE]Nuevo componente de reporte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,9 +25,14 @@ const routes: Routes = [
     path: "login",
     loadChildren: () =>
       import("./login/login.module").then((m) => m.LoginPageModule),
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'report/:id',
+    loadChildren: () => import('./report/report.module').then( m => m.ReportPageModule)
   },
 
 ];
